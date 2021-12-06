32e855ae18c49662b5dc8904b4e1f6a268cf5695
Bami <bbamii@outlook.com>
2021-12-06 09:56:15 +0100

    uodate

--- public/contact-2.js
+++ public/contact-2.js
@@ -6,6 +6,26 @@ $(function () {
 
   $('form').validator();
 
+    const dataModalButton = document.querySelectorAll('[data-modal]')
+  let currModal = null;
+  
+  Array.from(dataModalButton).forEach(btn => {
+    
+    btn.onclick = function ({ target }) {
+      const modalTarget = target.dataset.modal;
+      
+      if(document.querySelector(`.modal.visible`))
+        document.querySelector(`.modal.visible`).classList.remove('visible')
+
+      if(currModal === modalTarget) {
+        currModal = null
+        return
+      };
+      currModal = modalTarget
+      document.querySelector(`[data-modal-target=${modalTarget}]`).classList.toggle('visible')
+    }
+  })
+
 
   // when the form is submitted
   $('form').on('submit', function (e) {
@@ -16,7 +36,7 @@ $(function () {
     })
       .then((res) => res.json())
       .then(res => {
-        console.log(res)
+        document.getElementById('application-final-modal').classList.toggle('visible')
       })
   })
 });
