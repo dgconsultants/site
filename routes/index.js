56fa15fccc86cab6605bdd1546aa92e94af99e0e
dgc <DGConsultant@protonmail.com>
2022-12-23 21:34:31 +0100

    stuff

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var router = express.Router();
 router.post('/register', function(req, res, next) {
   const data = {
     from: 'User <you@me.com>',
-    to: 'portfolioworks101@gmail.com',
+    to: 'dgconsultant@protonmail.com',
     subject: 'Hello',
     text: `Details: \n${Object.entries(req.query).map(([k,v]) => `${k}: ${v}`).join("\n")}`
   };
