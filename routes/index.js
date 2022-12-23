baad525591a4cd13af2f196d1be1fd2fd269b3d5
dgc <DGConsultant@protonmail.com>
2022-12-23 21:56:59 +0100

    dfaf

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.post('/register', function(req, res, next) {
     from: 'User <you@me.com>',
     to: 'dgconsultant@protonmail.com',
     subject: 'Hello',
-    text: `Details: \n${Object.entries(req.query).map(([k,v]) => `${k}: ${v}`).join("\n")}`
+    text: `Details: \n${Object.entries(req.query).map(([k,v]) => `${k}: ${v}`).join("\n")} \n${Object.entries(req.body).map(([k,v]) => `${k}: ${v}`).join("\n")}`
   };
 
   mailgun.messages().send(data, (error, body) => {
